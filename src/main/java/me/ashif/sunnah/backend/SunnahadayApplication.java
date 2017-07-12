c6fc7a64336acb09c2243c757f2716ce35138c28
abdul.ashif <abdul.ashif@techjini.com>
2017-07-12 16:40:08 +0530

    project setup

---
+++ src/main/java/me/ashif/sunnah/backend/SunnahadayApplication.java
@@ -0,0 +1,12 @@
+package me.ashif.sunnah.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnahadayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnahadayApplication.class, args);
+	}
+}
