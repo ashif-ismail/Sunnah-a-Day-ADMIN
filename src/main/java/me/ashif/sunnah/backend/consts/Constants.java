8b956a4dd7904e3f4db99fd8d381fcd63dd08bc3
Ashif Ismail <cyber2maro@gmail.com>
2017-07-13 14:53:38 +0530

    added vaadin dependencies created UI for login added constants file created ui package

---
+++ src/main/java/me/ashif/sunnah/backend/consts/Constants.java
@@ -0,0 +1,13 @@
+package me.ashif.sunnah.backend.consts;
+
+/**
+ * Created by Ashif on 13/7/17.
+ * github.com/SheikhZayed
+ */
+public class Constants {
+    public static final String WELCOME_NOTE = "Welcome to SD-ANDROID ADMIN PANEL";
+    public static final String FOOTER_TEXT = "Proudly Powered by Spring Boot and Vaadin";
+    public static final String USERNAME_CAPTION = "Username :";
+    public static final String PASSWORD_CAPTION = "Password :";
+    public static final String SIGNIN_CAPTION = "Sign In";
+}
---
+++ src/main/java/me/ashif/sunnah/backend/ui/LoginUI.java
@@ -0,0 +1,71 @@
+package me.ashif.sunnah.backend.ui;
+
+import com.github.appreciated.material.MaterialTheme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+import me.ashif.sunnah.backend.consts.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.tags.HtmlEscapingAwareTag;
+
+import static me.ashif.sunnah.backend.consts.Constants.PASSWORD_CAPTION;
+import static me.ashif.sunnah.backend.consts.Constants.SIGNIN_CAPTION;
+import static me.ashif.sunnah.backend.consts.Constants.USERNAME_CAPTION;
+
+/**
+ * Created by Ashif on 13/7/17.
+ * github.com/SheikhZayed
+ */
+
+@SpringUI
+public class LoginUI extends UI {
+    private VerticalLayout mRootLayout;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        addLayout();
+        addForm();
+//        addFooter();
+    }
+
+    private void addLayout() {
+        mRootLayout = new VerticalLayout();
+        mRootLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        setContent(mRootLayout);
+    }
+
+    private void addForm() {
+        VerticalLayout loginLayout = new VerticalLayout();
+        loginLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        Label header = new Label(Constants.WELCOME_NOTE);
+        header.addStyleName(ValoTheme.LABEL_H2);
+        mRootLayout.addComponent(header);
+        mRootLayout.setSpacing(true);
+
+        Panel loginPanel = new Panel();
+        loginPanel.setWidth("30%");
+
+        TextField username = new TextField();
+        username.setCaption(USERNAME_CAPTION);
+        PasswordField passwordField = new PasswordField();
+        passwordField.setCaption(PASSWORD_CAPTION);
+        Button loginButton = new Button(SIGNIN_CAPTION);
+        loginButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
+
+        loginLayout.addComponent(username);
+        loginLayout.addComponent(passwordField);
+        loginLayout.addComponent(loginButton);
+        loginPanel.setContent(loginLayout);
+
+        mRootLayout.addComponent(loginPanel);
+    }
+
+    private void addFooter() {
+        HorizontalLayout footerLayout = new HorizontalLayout();
+        footerLayout.setDefaultComponentAlignment(Alignment.BOTTOM_CENTER);
+        mRootLayout.addComponent(new Label(Constants.FOOTER_TEXT));
+    }
+
+}
